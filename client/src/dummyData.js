8bc5215acc885fa84cc2eaeda25275bb11e4d0ad
Chris Honda <christopher.honda92@gmail.com>
2019-06-10 20:05:35 -0700

    Dummy data

---
+++ client/src/dummyData.js
@@ -0,0 +1,19 @@
+const books = [
+    {
+        title: "I Will Teach You To Be Rich",
+        author: "Ramit Sethi",
+        read: true
+    },
+    {
+        title: "The 4-Hour Work Week",
+        author: "Tim Ferriss",
+        read: true
+    },
+    {
+        title: "Cracking the Coding Interview",
+        author: "Gayle Laakmann McDowell",
+        read: false
+    }
+];
+
+export default books;
