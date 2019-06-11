a633e0cde9969d717e1cb884f3aac109a6357b8f
Chris Honda <christopher.honda92@gmail.com>
2019-06-10 20:59:36 -0700

    ListView component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
+import ListView from './components/ListView';
+import dummyData from 'dummyData';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  state = {
+    books: [],
+    selectedBook: {}
+  }
+
+  componentDidMount() {
+    this.setState({
+      books: dummyData
+    });
+  }
+
+  // selectBook = (id) => {
+  //   this.setState({
+  //     selectedBook
+  //   })
+  // }
+
+  render() {
+    return (
+      <div className="App">
+        <Route exact path='/' render={ (props) => { return(<ListView {...props} books={this.state.books} selectBook={this.selectBook} />)}} />
+      </div>
+    );
+  }
 }
 
 export default App;
